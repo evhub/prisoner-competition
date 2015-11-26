a8bd87b3acbea2c7ff66daed8207ef0e8afeeba0
Evan Hubinger <evanjhub@gmail.com>
2015-11-26 00:44:47 -0800

    Imports default bots

--- competition/__main__.py
+++ competition/__main__.py
@@ -1,43 +1,16 @@
 # IMPORTS:
 
-from prisoner.dilemma import *
-from prisoner.bots import *
 from prisoner.__main__ import main
+from prisoner.bots import cooperate_bot, defect_bot, tit_for_tat_bot, mirror_bot
 
 # SETUP:
 
-all_bots = {}
-
-# DEFAULT BOTS:
-
-@pd_bot
-def cooperate_bot(self_hist, opp_hist, opp_bot):
-    return True
-all_bots["cooperate_bot"] = cooperate_bot
-
-@pd_bot
-def defect_bot(self_hist, opp_hist, opp_bot):
-    return False
-all_bots["defect_bot"] = defect_bot
-
-@pd_bot
-def coin_flip_bot(self_hist, opp_hist, opp_bot):
-    return random.getrandbits(1)
-all_bots["coin_flip_bot"] = coin_flip_bot
-
-@pd_bot
-def tit_for_tat_bot(self_hist, opp_hist, opp_bot):
-    if opp_hist:
-        return opp_hist[-1]
-    else:
-        return True
-all_bots["tit_for_tat_bot"] = tit_for_tat_bot
-
-@pd_bot
-def mirror_bot(self_hist, opp_hist, opp_bot):
-    return opp_bot(opp_hist, self_hist, mirror_bot)
-mirror_bot += cooperate_bot
-all_bots["mirror_bot"] = mirror_bot
+all_bots = {
+    "cooperate": cooperate_bot,
+    "defect": defect_bot,
+    "tit_for_tat": tit_for_tat_bot,
+    "mirror": mirror_bot
+}
 
 # COMPETITION BOTS:
 
